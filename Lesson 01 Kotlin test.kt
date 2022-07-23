e448457cb6ff1c3bf2a2563aeee6f31b43183ffc
Serge Labada <sergelabada@gmail.com>
2022-07-24 00:13:41 +0300

    Android course IT-Academy

---
+++ Lesson 01 Kotlin test.kt
@@ -0,0 +1,55 @@
+package com.example.androidstudying
+
+fun main() {
+//    test()
+    check("Hello world", 'l')
+    val set = mutableSetOf(1, 2, 3, 3)
+    println(set)
+    val map = mapOf<Int, String>(
+        0 to "0",
+        1 to "1",
+        2 to "2"
+    )
+    val mutaMap = mutableMapOf<Int, String>(
+        0 to "0",
+        1 to "1",
+        2 to "2"
+    )
+    val test = Test(1)
+    test.doubleValue = 1.0
+    val user = User("first", "last", 20)
+    user.firstName = "new name"
+    println(user.fullName)
+}
+class User(var firstName: String, var lastName: String, val age: Int) {
+    val fullName
+        get() = "$firstName $lastName"
+}
+
+open class Test(var value: Int) {
+    var doubleValue: Double
+        get() = value.toDouble()
+        set(lue) {
+            this.value = lue.toInt()
+
+        }
+}
+
+fun test() {
+    val value1 = readLine()?.toInt() ?: return
+    val value2 = readLine()?.toInt() ?: return
+    when (value1 * value2) {
+        in 0..100 -> println("in range 0..100")
+        else -> println("out of range")
+    }
+}
+
+fun check(string: String, symbol: Char) {
+    var count = 0
+    for (char in string) {
+        if (char == symbol) {
+            count++
+        }
+    }
+    println(count)
+}
---
+++ Lesson 03 Kotlin test.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val num = "AaakbKJJKJHjh".numCap()
+    println(num)
+    println("AaakbKJJKJHjh".numCapitals)
+    println(listOf(1,2,3,4,5).filterBy { it > 2 })
+
+}
+
+fun String.numCap() = this.count { it.isUpperCase() }
+
+val String.numCapitals: Int
+    get() = count { it.isUpperCase() }
+
+fun List<Int>.filterBy(lam: (Int) -> Boolean): List<Int> {
+    return filter { lam(it) }
+}
