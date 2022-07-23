85e55cd990e3d795ea39c99b46b042b6c35b0880
Serge Labada <sergelabada@gmail.com>
2022-07-24 01:13:09 +0300

    Android course IT-Academy

--- Lesson 03 Kotlin test.kt
+++ Lesson 03 Kotlin test.kt
@@ -3,6 +3,10 @@ fun main() {
     println(num)
     println("AaakbKJJKJHjh".numCapitals)
     println(listOf(1,2,3,4,5).filterBy { it > 2 })
+    listOf(1,2,3,4,5,6)
+        .filterBy { it > 3 } //этот метод возвращает list
+        .dropLast(1)
+        .map { it * 2 } //смотреть сигнатуру метода
 }
 
 fun String.numCap() = this.count { it.isUpperCase() }
@@ -10,6 +14,25 @@ fun String.numCap() = this.count { it.isUpperCase() }
 val String.numCapitals: Int
     get() = count { it.isUpperCase() }
 
-fun List<Int>.filterBy(lam: (Int) -> Boolean): List<Int> {
-    return filter { lam(it) }
+fun List<Int>.filterBy(lam: (Int) -> Boolean): List<Int> { //более короткий вариант
+    filter(lam) //так можно, потому что у filter такая же сигнатура, как у нашей лямбды
+    return filter { lam(it) } //или так, более понятно
 }
+
+fun List<Int>.filterByLambda(lam: (Int) -> Boolean): List<Int> {
+    val list = mutableListOf<Int>()
+
+    for (item in this) { //более длинный вариант
+        if (lam(item)) {
+            list.add(item)
+        }
+    }
+
+    this.forEach { item -> //вариант получше, кстати, this здесь можно вообще не писать
+        if (lam(item)) {
+            list.add(item)
+        }
+    }
+    return list
+}
+
