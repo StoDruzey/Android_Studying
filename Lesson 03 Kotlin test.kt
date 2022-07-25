039e23aaa5c4b8276e44c1f46f70de6ef143700b
Serge Labada <sergelabada@gmail.com>
2022-07-26 00:39:58 +0300

    Android course IT-Academy

--- Lesson 03 Kotlin test.kt
+++ Lesson 03 Kotlin test.kt
@@ -18,6 +18,16 @@ fun main() {
             println("Find: $it")
             it == 5
         }
+
+    val rectangle = Rectangle(3.0, 4.0)
+    val square = Square(5.0)
+    val triangle = Triangle(6.0, 7.0)
+    rectangle.figureType()
+    println(rectangle.squareFig())
+    square.figureType()
+    println(square.squareFig())
+    triangle.figureType()
+    println(triangle.squareFig())
 }
 
 fun String.numCap() = this.count { it.isUpperCase() }
@@ -45,5 +55,35 @@ fun List<Int>.filterByLambda(lam: (Int) -> Boolean): List<Int> {
         }
     }
     return list
+}
+
+interface Squarable {
+    fun squareFig(): Double
+}
+abstract class Figure() : Squarable
+
+class Rectangle(val x: Double, val y: Double) : Figure() {
+    override fun squareFig(): Double {
+        return x * y
+    }
+}
+class Square(val x: Double) : Figure() {
+    override fun squareFig(): Double {
+        return x * x
+    }
+}
+class Triangle(val x: Double, val y: Double) : Figure() {
+    override fun squareFig(): Double {
+        return (x * y) / 2
+    }
+}
+
+fun Figure.figureType() {
+    when(this) {
+        is Rectangle -> println("Rectangle")
+        is Square -> println("Square")
+        is Triangle -> println("Triangle")
+        else -> println("Unknown figure")
+    }
 }
 
