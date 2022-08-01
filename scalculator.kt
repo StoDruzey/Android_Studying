0693971492d15d0bc67795051cf6c82295807a98
Serge Labada <sergelabada@gmail.com>
2022-08-01 12:30:02 +0300

    Daily commit

--- scalculator.kt
+++ scalculator.kt
@@ -1,6 +1,8 @@
+import androidx.core.text.isDigitsOnly
+
 fun main() {
-    var expression = getOperandsFromString(getData())
-    println(expression.toString())
+    action(getData())
+//    println(expression.toString())
 }
 
 fun getData(): List<String> {
@@ -16,64 +18,73 @@ fun getData(): List<String> {
             operands.add(it.value)
         }
     }
+//    operands.last().takeLast(1).
     return operands
 }
 
 fun action(ops: List<String>) {
     var operands = ops.toMutableList()
     var i: Int = 0
-    while (i < (operands.size - 1)) {
+    var size = operands.size
+    while (i < (size - 1)) {
+        println(operands)
         when (operands[i].takeLast(1)) {
             "*" -> {
                 val num1 = operands[i].dropLast(1).toDouble()
+//                if (i == size - 2) {
+//                    val num2 = operands[i + 1].dropLast(1).toDouble()
+//                }
                 val num2 = operands[i + 1].dropLast(1).toDouble()
-                val act2 = operands[i + 1].takeLast(1)
+                val act2 = if (i == size - 2) "" else operands[i + 1].takeLast(1)
                 operands[i + 1] = (num1 * num2).toString() + act2
                 operands.removeAt(i)
-
+                --size
+                --i
             }
+            "/" -> {
+                val num1 = operands[i].dropLast(1).toDouble()
+                val num2 = operands[i + 1].dropLast(1).toDouble()
+                val act2 = if (i == size - 2) "" else operands[i + 1].takeLast(1)
+                operands[i + 1] = (num1 / num2).toString() + act2
+                operands.removeAt(i)
+                --size
+                --i
+            }
+//            else ->
         }
+        ++i
     }
-    operands.forEachIndexed() { index, element ->
-        while (index < operands.size - 1) {
-            when (element.takeLast(1)) {
-                "*" -> {
-                    val num1 = element.dropLast(1).toDouble()
-                    val num2 = element[index + 1].
-                }
-            }
-        }
-
-
+    println(ops)
+    println(operands)
 }
 
 
-data class Expression(var x: Number = 0, var y: Number = 0, var action: String = "")
+//data class Expression(var x: Number = 0, var y: Number = 0, var action: String = "")
 
-fun getOperandsFromString(string: String): Expression {
-    var expression = Expression()
-    var operand1: String = ""
-    var operand2: String = ""
-    var action: String = ""
-
-
-//        var (operand1, action, operand2) = string.split(" ")
+//fun getOperandsFromString(string: String): Expression {
+//    var expression = Expression()
+//    var operand1: String = ""
+//    var operand2: String = ""
+//    var action: String = ""
 //
-//        if (action in "+-*/") {
-//            expression.action = action
-//        }
 //
-//        if (operand1.contains('.')) {
-//            expression.x = operand1.toDouble()
-//        } else {
-//            expression.x = operand1.toInt()
-//        }
-//
-//        if (operand2.contains('.')) {
-//            expression.y = operand2.toDouble()
-//        } else {
-//            expression.y = operand2.toInt()
-//        }
-    }
-    return expression
-}
+////        var (operand1, action, operand2) = string.split(" ")
+////
+////        if (action in "+-*/") {
+////            expression.action = action
+////        }
+////
+////        if (operand1.contains('.')) {
+////            expression.x = operand1.toDouble()
+////        } else {
+////            expression.x = operand1.toInt()
+////        }
+////
+////        if (operand2.contains('.')) {
+////            expression.y = operand2.toDouble()
+////        } else {
+////            expression.y = operand2.toInt()
+////        }
+//    }
+//    return expression
+//}
